d17f4b85e5aabd3ebbb5730af35372bf851ac1ee
Shivam Jamaiwar <shivamjamaiwar196@gmail.com>
2019-03-02 00:40:41 +0530

    Add model class

---
+++ src/model/Course.java
@@ -0,0 +1,43 @@
+package model;
+
+public class Course {
+	
+	private int id;
+	private String courseName;
+	private String branch;
+	private double fees;
+	private String time;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	public double getFees() {
+		return fees;
+	}
+	public void setFees(double fees) {
+		this.fees = fees;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	
+	
+
+}
--- src/model/Employee.java
+++ src/model/Employee.java
@@ -7,5 +7,24 @@ public class Employee extends Person{
 	private String profession;
 	private double salary;
 	private Date dateOfJoining;
+	public String getProfession() {
+		return profession;
+	}
+	public void setProfession(String profession) {
+		this.profession = profession;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public Date getDateOfJoining() {
+		return dateOfJoining;
+	}
+	public void setDateOfJoining(Date dateOfJoining) {
+		this.dateOfJoining = dateOfJoining;
+	}
+	
 	
 }
